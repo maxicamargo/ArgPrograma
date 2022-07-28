01f00b4f2dc0f26daf36e877c741b070e962d260
Maximiliano Camargo <maxi.camargo@gmail.com>
2022-07-28 11:15:00 -0300

    Estructura basica Spring Boot

---
+++ BackEnd/src/test/java/com/porfolio/mjc/MjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.mjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
