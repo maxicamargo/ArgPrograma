6ac1beeb5581d6087e94480d5863beebe9f7377c
Maximiliano Camargo <maxi.camargo@gmail.com>
2022-07-28 21:42:18 -0300

    Primer interaccion entre el FrontEnd y el BackEnd, configuracion del nombre y apellidos desde la base

--- BackEnd/src/main/java/com/porfolio/mjc/controller/Controller.java
+++ BackEnd/src/main/java/com/porfolio/mjc/controller/Controller.java
@@ -5,6 +5,7 @@ import com.porfolio.mjc.service.IPersonaService;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
     
     @Autowired
@@ -74,6 +76,9 @@ public class Controller {
         return persona;
     }
     
-    
+    @GetMapping ("/ver/{id}")
+    public Persona verPersona(@PathVariable Long id){
+        return persoServ.buscarPersona(id);
+    }
     
 }
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftComponent } from './components/hard-soft/hard-soft.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -33,7 +33,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
