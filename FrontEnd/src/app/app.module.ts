75707ba7214813c477ce95f8b37b090e6ad58b6d
Maximiliano Camargo <maxi.camargo@gmail.com>
2022-07-28 22:12:10 -0300

    Routering con Angular - Seccion Login generica

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HardSoftComponent } from './components/hard-soft/hard-soft.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HardSoftComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
